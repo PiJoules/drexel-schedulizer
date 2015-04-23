d4aa186e7af65454484e752b43146d9cae97f937
Leo is awesome <lchan1994@yahoo.com>
2015-04-23 13:42:46 -0400

    this is going in a crobjob l8r

--- index.php
+++ index.php
@@ -23,81 +23,96 @@ foreach($terms as $term){
 	$subjects = $termLinkHTML->find(".collegePanel .odd a, .collegePanel .even a"); // second layer of links to courses
 
 	// for each term, get the college
-	
+	$colleges = $termLinkHTML->find("#sideLeft a"); // second layer of links to each college
+	$collegeLinks = array();
+	foreach($colleges as $college) {
+		$collegeLink = html_entity_decode($baseURL . $college->href);
+		$collegeName = html_entity_decode($college->plaintext);
 
-	// for each college, get subject
+		// for each college, get subject
+		$subjectLinks = array();
+		foreach($subjects as $subject){
+			$subjectLink = html_entity_decode($baseURL . $subject->href);
+			$subjectText = html_entity_decode($subject->plaintext);
 
-
-	$subjectLinks = array();
-	foreach($subjects as $subject){
-		$subjectLink = html_entity_decode($baseURL . $subject->href);
-		$subjectText = html_entity_decode($subject->plaintext);
-
-		// for each subject, get the courses
-		$subjectLinkHTML = makeSimpleHTMLDOM($subjectLink);
-		$subjectLinkHTML->set_callback("removeComments");
-		$subjectLinkHTML = str_get_html($subjectLinkHTML . "");
-		$courses = $subjectLinkHTML->find("tr.odd, tr.even");
-		
-		$coursesInfo = array();
-		foreach($courses as $i => $course){
-			/**
-			 * subject code | course no. | instruction type | instruction method | section | crn   | course title                       | days/time             | instructor
-			 * -------------------------------------------------------------------------------------------------------------------------------------------------------------
-			 * ECEC         | 301        | Lecture          | Face to Face       | 001     | 41331 | Advanced Programming for Engineers | R 06:30 pm - 09:20 pm | STAFF
-			 */
+			// for each subject, get the courses
+			$subjectLinkHTML = makeSimpleHTMLDOM($subjectLink);
+			$subjectLinkHTML->set_callback("removeComments");
+			$subjectLinkHTML = str_get_html($subjectLinkHTML . "");
+			$courses = $subjectLinkHTML->find("tr.odd, tr.even");
 			
-			// I also catch nested tr tags with "odd" and "even" classes, the ones I want have 9 children that are td tags
-			if (count($course->children()) < 9)
-				continue;
-			
-			$subjectCode = $course->children(0)->plaintext;
-			$courseNumber = intval($course->children(1)->plaintext);
-			$instructionType = html_entity_decode($course->children(2)->plaintext);
-			$instructionMethod = $course->children(3)->plaintext;
-			$section = $course->children(4)->plaintext; // 061, 601, A, B, etc.
-			$crn = intval($course->children(5)->plaintext);
-			$courseTitle = html_entity_decode($course->children(6)->plaintext);
-			$days = daysTableToArray($course->children(7));
-			$instructor = $course->children(8)->plaintext;
+			$coursesInfo = array();
+			foreach($courses as $i => $course){
+				/**
+				 * subject code | course no. | instruction type | instruction method | section | crn   | course title                       | days/time             | instructor
+				 * -------------------------------------------------------------------------------------------------------------------------------------------------------------
+				 * ECEC         | 301        | Lecture          | Face to Face       | 001     | 41331 | Advanced Programming for Engineers | R 06:30 pm - 09:20 pm | STAFF
+				 */
+				
+				// I also catch nested tr tags with "odd" and "even" classes, the ones I want have 9 children that are td tags
+				if (count($course->children()) < 9)
+					continue;
+				
+				$subjectCode = $course->children(0)->plaintext;
+				$courseNumber = intval($course->children(1)->plaintext);
+				$instructionType = html_entity_decode($course->children(2)->plaintext);
+				$instructionMethod = $course->children(3)->plaintext;
+				$section = $course->children(4)->plaintext; // 061, 601, A, B, etc.
+				$crn = intval($course->children(5)->plaintext);
+				$courseTitle = html_entity_decode($course->children(6)->plaintext);
+				$days = daysTableToArray($course->children(7));
+				$instructor = $course->children(8)->plaintext;
 
-			$courseInfo = array(
-				"subjectCode" => $subjectCode,
-				"courseNumber" => $courseNumber,
-				"instructionType" => $instructionType,
-				"instructionMethod" => $instructionMethod,
-				"section" => $section,
-				"crn" => $crn,
-				"courseTitle" => $courseTitle,
-				"days" => $days,
-				"instructor" => $instructor
+				$courseInfo = array(
+					"subjectCode" => $subjectCode,
+					"courseNumber" => $courseNumber,
+					"instructionType" => $instructionType,
+					"instructionMethod" => $instructionMethod,
+					"section" => $section,
+					"crn" => $crn,
+					"courseTitle" => $courseTitle,
+					"days" => $days,
+					"instructor" => $instructor
+				);
+
+				array_push($coursesInfo, $courseInfo);
+			}
+			/*$fp = fopen('results.json', 'w');
+			fwrite($fp, json_encode($coursesInfo, JSON_PRETTY_PRINT));
+			fclose($fp);
+			var_dump($coursesInfo);
+			return;*/
+
+			$subjectLinkArray = array(
+				"link" => $subjectLink,
+				"subject" => $subjectText,
+				"courses" => $coursesInfo
 			);
-
-			array_push($coursesInfo, $courseInfo);
+			array_push($subjectLinks, $subjectLinkArray);
 		}
 		/*$fp = fopen('results.json', 'w');
-		fwrite($fp, json_encode($coursesInfo, JSON_PRETTY_PRINT));
+		fwrite($fp, json_encode($subjectLinks, JSON_PRETTY_PRINT));
 		fclose($fp);
-		var_dump($coursesInfo);
+		var_dump($subjectLinks);
 		return;*/
 
-		$subjectLinkArray = array(
-			"link" => $subjectLink,
-			"subject" => $subjectText,
-			"courses" => $coursesInfo
+		$collegeLinkArray = array(
+			"link" => $collegeLink,
+			"college" => $collegeName,
+			"subjects" => $subjectLinks
 		);
-		array_push($subjectLinks, $subjectLinkArray);
+		array_push($collegeLinks, $collegeLinkArray);
 	}
 	$fp = fopen('results.json', 'w');
-	fwrite($fp, json_encode($subjectLinks, JSON_PRETTY_PRINT));
+	fwrite($fp, json_encode($collegeLinks, JSON_PRETTY_PRINT));
 	fclose($fp);
-	var_dump($subjectLinks);
+	var_dump($collegeLinks);
 	return;
 
 	$termLinkArray = array(
 		"link" => $termLink,
 		"term" => $term->plaintext,
-		"subjects" => $subjectLinks
+		"colleges" => $collegeLinks
 	);
 	array_push($termLinks, $termLinkArray);
 }
